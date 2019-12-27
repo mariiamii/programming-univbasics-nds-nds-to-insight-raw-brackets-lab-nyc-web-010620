14d6bce6b7f217e33cbe8211dc86e1053c85714e
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-27 04:11:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,12 +24,12 @@ def directors_totals(nds)
   total = {}
   
   while directors_index < nds.length do
-    name = nds[directors_index][:name]
-    total[name] = 0
+    directors_name = nds[directors_index][:name]
+    total[directors_name] = 0
     movie_index = 0
     
     while movie_index < nds[directors_index][:movies].length do 
-      total[name] += nds[directors_index][:movies][movie_index][:worldwide_gross]
+      total[directors_name] += nds[directors_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1
     end
     directors_index += 1
